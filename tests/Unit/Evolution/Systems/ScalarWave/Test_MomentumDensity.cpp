a1f961f4e09921c3021d32480dfcc6cc9ed53dd5
acifajkya <a.ceja4@csu.fullerton.edu>
2022-07-07 11:31:37 -0700

    Add Tag Declarations and MomentumDensity.py test

--- tests/Unit/Evolution/Systems/ScalarWave/Test_MomentumDensity.cpp
+++ tests/Unit/Evolution/Systems/ScalarWave/Test_MomentumDensity.cpp
@@ -11,14 +11,50 @@
 #include "DataStructures/DataVector.hpp"
 #include "DataStructures/Tensor/Tensor.hpp"
 #include "Evolution/Systems/ScalarWave/MomentumDensity.hpp"
+#include "Evolution/Systems/ScalarWave/Tags.hpp"
 #include "Framework/CheckWithRandomValues.hpp"
 #include "Framework/SetupLocalPythonEnvironment.hpp"
 #include "Helpers/DataStructures/DataBox/TestHelpers.hpp"
 #include "Helpers/DataStructures/MakeWithRandomValues.hpp"
 
-#include <iostream>
+namespace {
+template <size_t SpatialDim, typename DataType>
+void test_compute_item_in_databox(const DataType& used_for_size) {
+  TestHelpers::db::test_compute_tag<
+      ScalarWave::Tags::MomentumDensityCompute<SpatialDim>>("MomentumDensity");
 
-namespace {}  // namespace
+  MAKE_GENERATOR(generator);
+  std::uniform_real_distribution<> dist(-1., 1.);
+
+  const tnsr::i<DataVector, SpatialDim, Frame::Inertial> pi =
+      make_with_random_values<tnsr::i<DataVector, SpatialDim, Frame::Inertial>>(
+          make_not_null(&generator), dist, used_for_size);
+  const tnsr::i<DataVector, SpatialDim, Frame::Inertial> phi =
+      make_with_random_values<tnsr::i<DataVector, SpatialDim, Frame::Inertial>>(
+          make_not_null(&generator), dist, used_for_size);
+
+  const auto box = db::create<
+      db::AddSimpleTags<ScalarWave::Pi, ScalarWave::Phi<SpatialDim>>,
+      db::AddComputeTags<ScalarWave::Tags::MomentumDensityCompute<SpatialDim>>>(
+      pi, phi);
+
+  const auto expected = ScalarWave::momentum_density(pi, phi);
+
+  CHECK_ITERABLE_APPROX(
+      (db::get<ScalarWave::Tags::MomentumDensity<SpatialDim>>(box)), expected);
+}
+
+template <size_t SpatialDim>
+void test_momentum_density(const DataVector& used_for_size) {
+  void (*f)(
+      const gsl::not_null<tnsr::i<DataVector, SpatialDim, Frame::Inertial>*>,
+      const tnsr::i<DataVector, SpatialDim, Frame::Inertial>&,
+      const tnsr::i<DataVector, SpatialDim, Frame::Inertial>&) =
+      &ScalarWave::momentum_density<SpatialDim>;
+  pypp::check_with_random_values<1>(f, "MomentumDensity", {"momentum_density"},
+                                    {{{-1., 1.}}}, used_for_size);
+}
+}  // namespace
 
 SPECTRE_TEST_CASE("Unit.Evolution.Systems.ScalarWave.MomentumDensity",
                   "[Unit][Evolution]") {
